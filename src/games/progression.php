6c4b3a408b8b518c744b9a253e07819bdb1e918d
kiirt <entkirrt@gmail.com>
2020-02-12 17:00:42 +0300

    Add brain-progression

---
+++ src/games/progression.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace BrainGames\games\progression;
+
+use function BrainGames\engine\play;
+
+define('PROGRESSION_LENGTH', 10);
+define('RULE_PROGRESSION', 'What number is missing in the progression?');
+
+function run()
+{
+    play(
+        function () {
+            $startElementPosition = rand(1, 5);
+            $step = rand(1, PROGRESSION_LENGTH);
+            $hiddenElementPosition = rand(0, PROGRESSION_LENGTH - 1);
+            $progression = [];
+            for ($i = 0; $i < PROGRESSION_LENGTH; $i++) {
+                $progression[] = $startElementPosition + $step * $i;
+            }
+            $answer = $progression[$hiddenElementPosition];
+            $progression[$hiddenElementPosition] = '..';
+            $question = implode(" ", $progression);
+            return [$question, $answer];
+        },
+        RULE_PROGRESSION
+    );
+}
