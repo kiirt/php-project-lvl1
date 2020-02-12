f46c5af10ea02d0c0909e8c5058e236242014d94
kiirt <entkirrt@gmail.com>
2020-02-12 17:50:10 +0300

    Add game brain-prime

---
+++ src/games/prime.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace BrainGames\games\prime;
+
+use function BrainGames\engine\play;
+
+define(
+    'RULE_PRIME',
+    'Answer "yes" if given number is prime. Otherwise answer "no".'
+);
+
+function run()
+{
+    play(
+        function () {
+            $question = rand(1, 100);
+            $answer = isPrime($question) ? 'yes' : 'no';
+            return [$question, $answer];
+        },
+        RULE_PRIME
+    );
+}
+
+function isPrime($number)
+{
+    if ($number == 2) {
+        return true;
+    }
+    if ($number < 2 || $number % 2 == 0) {
+        return false;
+    }
+    $i = 3;
+    while ($i <= (int) sqrt($number)) {
+        if ($number % $i == 0) {
+            return false;
+        }
+        $i += 2;
+    }
+    return true;
+}
